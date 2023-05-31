663257e53048c0a2bcfa06cb733f4a1b44aebf06
suzuki <silverface8888@gmail.com>
2023-05-31 16:49:23 +0900

    LinePay

---
+++ app/Http/Controllers/Auth/LinePayController.php
@@ -0,0 +1,134 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class LinePayController extends Controller
+{
+
+    private $channel_id;
+    private $channel_secret_key;
+
+    public function __construct()
+    {
+       // $this->channel_id = '1657931616';
+        $this->channel_id = config('app.public_key');
+       // $this->channel_secret_key = 'df4edbd305efbf8f09be3e041b5b73af';
+        $this->channel_secret_key = config('app.secret_key');
+    }
+
+    public function index(){
+    
+        // LINE URL ※下記どちらでも可能
+        $line_url = 'https://sandbox-api-pay.line.me';
+        // $line_url = 'https://api-pay.line.me';
+
+        // パス
+        $path = '/v3/payments/request';
+        
+        // ノンス
+        $nonce = $this->gen_uuid();
+
+        // ボディ 
+        $body = json_encode(array(
+            "amount" => 1,
+            "currency" => "JPY",
+            "orderId" => "testOrderId",
+            "packages" => array(array(
+                "id" => "testPackages1",
+                'name'   => "packages",
+                "amount" => 1,
+                "products" => array(array(
+                    "id" => "testProducts1",
+                    "name" => "products",
+                    "quantity" => 1,
+                    "price" => 1
+                ))
+            )),
+            "redirectUrls" => array(
+               // "confirmUrl" => "https://myPage",
+                "confirmUrl" => "http://localhost/dashboard",
+                
+                "cancelUrl" => "https://myPage"
+            )
+        ));
+        
+    //    echo $body;
+        
+        // シグネチャ
+        $signature = base64_encode(hash_hmac('sha256', $this->channel_secret_key . $path . $body . $nonce, $this->channel_secret_key, true));
+        
+        // ヘッダ情報
+        $header = array(
+            'Content-Type:'               . 'application/json',
+            'X-LINE-ChannelId:'           . $this->channel_id,
+            'X-LINE-Authorization-Nonce:' . $nonce,
+            'X-LINE-Authorization:'       . $signature
+        );
+        
+        /***********************************************************
+         * API実行
+         ***********************************************************/
+        $curl = curl_init();
+        
+        curl_setopt($curl, CURLOPT_URL,            $line_url . $path);
+        curl_setopt($curl, CURLOPT_HTTPHEADER,     $header);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_POST,           true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS,     $body);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        /*
+        echo '<br><br>';
+        print_r($result) ;
+        */
+
+        $arr=json_decode($result,true);
+
+        /*
+        echo '<br><br>';
+        print_r($arr['returnCode']) ;
+        echo '<br><br>';
+        print_r($arr['info']) ;
+        echo '<br><br>';
+        */
+
+      //  print_r($arr['info']['paymentUrl']['web']) ;
+
+      //  return redirect()->away('https://www.google.com');
+
+        return Redirect()->away($arr['info']['paymentUrl']['web']);
+
+    }
+
+    /**
+     * UUIDを生成する
+     */
+    public function gen_uuid()
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
+    }
+
+}
+
+
+
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,36 @@ import PrimaryButton from '../Components/PrimaryButton.vue';
             </div>
         </div>
 
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
+                <header>
+                    <h2 class="text-lg font-medium text-gray-900">
+                        LINE PAY
+                    </h2>
+                </header>
+
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <PrimaryButton   onClick="window.open('/payment/linepay/geturl/1','_blank','')">
+                            LINE Payで支払う
+                        </PrimaryButton>
+                    </div>
+                </div>
+
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <div>
+                            <PrimaryButton>
+                                <a href="/payment/linepay/geturl/1" target="_blank">LINE Payで支払う</a>
+                            </PrimaryButton>
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
         
     </AuthenticatedLayout>
 </template>
