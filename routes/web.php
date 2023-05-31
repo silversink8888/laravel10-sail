bd7955181f7dcc8aaa8c0217ab6c0e905d077f73
suzuki <silverface8888@gmail.com>
2023-05-31 17:34:33 +0900

    LinePay

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Auth\LinePayController;
 
 
 /*
@@ -37,3 +38,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+//Route::get('/payment/linepay/geturl', 'LinePayController@getLinepayPaymentURL');
+Route::get('/payment/linepay/geturl/{id}',         [LinePayController::class, 'index']);
+
+Route::get('/payment/linepay/paidSuccess', 'LinePayController@successLinepayPayment');
